89a9e24f06e5d227dbd65b8114252fcc9bd8b0dc
Muhammad <mh5458837@gmail.com>
2026-03-03 10:15:18 +0700

    feat: Database Setup

---
+++ db/schema.js
@@ -0,0 +1,15 @@
+import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+
+export const students = pgTable("students", {
+    id: serial("id").primaryKey(),
+    name: varchar("name", { length: 255 }).notNull(),
+    gender: varchar("gender", { length: 10 }).notNull(),
+    memorize: integer("memorize").default(0),
+    parent: varchar("parent", { length: 255 }).notNull()
+});
+
+export const admins = pgTable("admins", {
+    id: serial("id").primaryKey(),
+    username: varchar("username", { length: 255 }).notNull().unique(),
+    password: varchar("password", { length: 255 }).notNull()
+});
