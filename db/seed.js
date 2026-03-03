c4d4cc268f41ab01cabae0975b1e37c2f68bb9db
Muhammad <mh5458837@gmail.com>
2026-03-03 10:34:20 +0700

    feat: update seed and index file

--- db/seed.js
+++ db/seed.js
@@ -8,23 +8,28 @@ const sql = postgres(process.env.DATABASE_URL);
 const db = drizzle(sql);
 
 async function seed() {
-    console.log("Seeding database...");
-
-    await db.insert(students).values({
-        name: "John Doe",
-        gender: "male",
-        memorize: 5,
-        parent: "Doe Family"
-    })
+    try {
+        console.log("Seeding database...");
     
-    const hashed = await bcrypt.hash("admin123", 10);
-    await db.insert(admins).values({
-        username: "admin",
-        password: hashed
-    })
-
-    console.log("Database seeded successfully!");
-    process.exit(0);
+        await db.insert(students).values({
+            name: "John Doe",
+            gender: "male",
+            memorize: 5,
+            parent: "Doe Family"
+        })
+        
+        const hashed = await bcrypt.hash("admin123", 10);
+        await db.insert(admins).values({
+            username: "admin",
+            password: hashed
+        })
+    
+        console.log("Database seeded successfully!");
+    } catch (error) {
+        console.error("Error seeding database:", error);
+    } finally {
+        process.exit(0);
+    }
 }
 
 seed();
